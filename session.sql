8b1a6309cb169c01297c2b92bf8e96c9155c0c64
Kyle <klougnot@gmail.com>
2016-03-02 15:54:05 -0500

    Create session.sql
    Table to create sessions in PHP mysqli

---
+++ session.sql
@@ -0,0 +1,8 @@
+#Table to create Sessions
+
+CREATE TABLE `session` (
+  `Session_Id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Session_Expires` datetime NOT NULL,
+  `Session_Data` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`Session_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
